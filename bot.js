17b0c116e00638404a406e8d2d194f04d9231d10
PrinceCrafter <35907951+PrinceCrafter@users.noreply.github.com>
2018-07-21 20:03:45 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -785,4 +785,4 @@ client.on('message', message => {
            }
 });
 
-client.login('NDcwMjQ0MjIwODI5OTU4MTQ1.DjTc8A.9vs3jG31wNWNzMAd6ykUTBcLSeI')
+client.login('NDY5NTM5MTUyOTY3MzY4NzE0.DjUTnw.ANYssf1J_y-bWI-Zqpn5HtJF0MI')
